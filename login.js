42df85203dae3f76cafc6039aa369937714a1928
mahmoud843 <138847197+mahmoud843@users.noreply.github.com>
2024-09-24 02:01:10 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,18 @@
+function loginUser(event) {
+    event.preventDefault();
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+  
+    if (!username || !password) {
+        document.getElementById('message').innerText = 'Error! Please enter username and password';
+        return; 
+    }
+  
+    if (users[username] && users[username].password === password) {
+        document.getElementById('message2').innerText = "Please enter correct username and password.";
+        
+    } else {
+        document.getElementById('message').innerText = "Account created successfully.";
+        window.location.href = "html.html"; 
+    }
+  }
